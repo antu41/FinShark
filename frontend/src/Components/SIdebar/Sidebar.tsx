a1db4f260ec6b7713bdea75e5d95dbc4199f589f
Antu <antudas444475@email.com>
2025-10-12 18:31:03 +0600

    Created Dashboard Components

--- frontend/src/Components/SIdebar/Sidebar.tsx
+++ frontend/src/Components/SIdebar/Sidebar.tsx
@@ -20,7 +20,7 @@ const Sidebar = (props: Props) => {
               <FaHome />
               <h6 className="ml-3">Company Profile</h6>
             </Link>
-                        <Link
+            <Link
               to="income-statement"
               className="flex md:min-w-full text-blueGray-500 text-medium uppercase font-bold block pt-1 pb-4 no-underline"
             >
---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,18 @@
+import Table from "../../Components/Table/Table";
+
+interface Props {}
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+      <h1>FinShark Design Page</h1>
+      <h2>
+        This is FinShark's design page. This is where we will house various
+        design aspects of the app
+      </h2>
+      <Table />
+    </>
+  );
+};
+
+export default DesignPage;
